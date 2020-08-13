dcd45bf00101742d3adf291df9df8ffd99469f61
payhache <payhache@gmail.com>
2020-08-13 10:02:45 +0200

    vue home ok

--- view/home_view.php
+++ view/home_view.php
@@ -1,3 +1,27 @@
-<?php
+<?php include_once('view\parts\meta.html') ?>
 
-var_dump($cars);
+<table class="table">
+    <thead>
+    <td> # </td>
+    <td>Marque</td>
+    <td>Modele</td>
+    <td>Action</td>
+    </thead>
+
+    <tbody>
+    <?php
+    foreach ($cars as $car) {
+        ?>
+        <tr>
+            <td><?php echo $car->getId()?></td>
+            <td><?php echo $car->getMarque()?></td>
+            <td><?php echo $car->getModele()?></td>
+            <td>
+                ongoing
+            </td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
